a0eee9e64b1c06e64939d11ffdcfc8fba88269ee
Hamdi Warren <warren.hamdi@gmail.com>
2025-03-09 10:08:59 +0100

    [Embedded] -> module03 ex00 -> set port to output rgb led

--- module03/ex00/src/main.c
+++ module03/ex00/src/main.c
@@ -1,7 +1,21 @@
-#define RGB_LED PD5
-int main(void){
-	DDRB |= (1 << RGB_LED);
-	TCCR1B |= (1  << WGM12);
 
+#include <avr/io.h>
+#define RGB_LED_RED PD5
+#define RGB_LED_BLUE PD3
+#define RGB_LED_GREEN PD6
+int main(void){
+	
+	DDRD |= (1 << RGB_LED_RED);
+	PORTD |= (1 << RGB_LED_RED);
+	DDRD |= (1 << RGB_LED_BLUE);
+	PORTD |= (1 << RGB_LED_BLUE);
+	DDRD |= (1 << RGB_LED_GREEN);
+	PORTD |= (1 << RGB_LED_GREEN);
 	while(1);
 }
+/*• T1/OC0B/PCINT21 – Port D, Bit 5
+T1, Timer/Counter1 counter source.
+OC0B, output compare match output: The PD5 pin can serve as an external output for the Timer/Counter0 compare match
+B. The PD5 pin has to be configured as an output (DDD5 set (one)) to serve this function. The OC0B pin is also the output
+pin for the PWM mode timer function.
+PCINT21: Pin change interrupt source 21. The PD5 pin can serve as an external interrupt source*/
